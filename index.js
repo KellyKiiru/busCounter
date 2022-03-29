e4855115016fc5c3523083923245b32ca02f97b6
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-03-29 09:50:39 +0300

    fix bugs to bus counter app

--- index.js
+++ index.js
@@ -4,18 +4,25 @@ let count = 0
 
 function increment() {
     count = count + 1
-    counter.innerText = count
+    counter.textContent = count
 }
 
 function decrement() {
     count = count - 1
-    counter.innerText = count
+    counter.textContent = count
     if (count == 0 || count < 0) {
-        counter.innerText = 0
+        counter.textContent = 0
     }
 
 }
 
+let saveEl = document.getElementById('save-el')
+
+
 function save() {
+    let previousEntry = count + " - "
+    saveEl.textContent += ' ' + previousEntry
     console.log(count)
+    count = 0
+    counter.textContent = 0
 }
